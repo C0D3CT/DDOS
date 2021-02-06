2ef374b039da67ddeaa3c8d6af7dd94ad5d46e80
Crocodile <72583503+C0D3CT@users.noreply.github.com>
2021-02-06 12:04:20 +0330

    Add files via upload

---
+++ install-debian.sh
@@ -0,0 +1,9 @@
+clear
+cd
+sudo apt-get install figlet -y
+sudo apt-get install ruby -y
+sudo gem install lolcat
+figlet installing... | lolcat
+sudo apt-get install python -y
+cd DDOS
+python2 ddos-attack.py
